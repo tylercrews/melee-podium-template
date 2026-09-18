e0e08cf40b19c9f78b9ee39d00c6f5923e065011
tylercrews <tycrews64@gmail.com>
2026-09-18 18:52:27 -0400

    fixing the new stylized version of the flat segmentation mask

--- test/colorize_podium_test.py
+++ test/colorize_podium_test.py
@@ -693,7 +693,7 @@ def main() -> None:
                 FIRST_PLACE_BOX.exterior_line,
                 FIRST_PLACE_BOX.interior_line,
                 panel_classes=((0, 255, 255),)
-                if size_name == "03_medium"
+                if size_name in ("00_flat", "03_medium")
                 else ((255, 0, 0),),
             ).save(output_path)
             print(f"Generated {output_path}")
