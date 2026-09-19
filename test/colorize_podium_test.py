d8d3d2ecccd6e3a2f65e8cd07fd03023bc0334c2
tylercrews <tycrews64@gmail.com>
2026-09-19 02:30:52 -0400

    fixed tall segmentation mask

--- test/colorize_podium_test.py
+++ test/colorize_podium_test.py
@@ -45,7 +45,7 @@ PODIUM_SIZES: tuple[tuple[str, str, str], ...] = (
         "02_short.png",
     ),
     ("03_medium", "03x_medium_segmentation_mask.png", "03_medium.png"),
-    ("04_tall", "04x_tall_segmentation_mask.png", "04_tall.png"),
+    ("04_tall", "04x_tall_segmentation_mask_cleaned.png", "04_tall.png"),
     ("05_x_tall", "05x_x_tall_segmentation_mask.png", "05_x_tall.png"),
 )
 
@@ -739,7 +739,8 @@ def main() -> None:
                 FIRST_PLACE_BOX.exterior_line,
                 FIRST_PLACE_BOX.interior_line,
                 panel_classes=((0, 255, 255),)
-                if size_name in ("00_flat", "01_x_short", "02_short", "03_medium")
+                if size_name
+                in ("00_flat", "01_x_short", "02_short", "03_medium", "04_tall")
                 else ((255, 0, 0),),
                 precleaned=precleaned,
             ).save(output_path)
